15b34086401806c5962574e1b0bd52d72f620c21
Dan Hedges <daniel.a.hedges@gmail.com>
2015-10-25 14:12:16 -0500

    Corrected foreign keys

--- sql/CreateTables.sql
+++ sql/CreateTables.sql
@@ -1,40 +1,32 @@
-BEGIN;
 
--- Table 1
-CREATE TABLE user
-( user_id number(10) NOT NULL,
-  user_name varchar(50) NOT NULL,
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+CREATE TABLE users
+( user_id INT PRIMARY KEY,
+  user_name varchar(50) NOT NULL
 );
 
--- Table 2
-CREATE TABLE BloodSugar
-( blood_sugar_id number(6) NOT NULL,
-  user_id number(10) NOT NULL,
+CREATE TABLE blood_sugar
+( blood_sugar_id INT PRIMARY KEY,
+  user_id INT NOT NULL,
   measurement varchar(10) NOT NULL,
-  CONSTRAINT blood_sugar_pk PRIMARY KEY (blood_sugar_id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
--- Table 3
 CREATE TABLE meals
 ( meal_ID INT PRIMARY KEY,
+  user_id INT NOT NULL,
   premeal_blood_sugar_id number(6),
   meal_time datetime,
   est_carbs number(10),
   Insulin number(10),
   postmeal_blood_sugar_id number(6),
-  CONSTRAINT fk_bloodsugar1
-    FOREIGN KEY (premeal_blood_sugar_id)
-    REFERENCES BloodSugar(blood_sugar_id),
-  CONSTRAINT fk_bloodsugar2
-    FOREIGN KEY (postmeal_blood_sugar_id)
-    REFERENCES BloodSugar(blood_sugar_id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  FOREIGN KEY (premeal_blood_sugar_id) REFERENCES blood_sugar(blood_sugar_id),
+  FOREIGN KEY (postmeal_blood_sugar_id) REFERENCES blood_sugar(blood_sugar_id)
 );
 
---Table 4
-CREATE TABLE Photos (
-  user_id number(10) NOT NULL,
-  data BLOB
+CREATE TABLE photos (
+  photo_id INT PRIMARY KEY,
+  user_id INT NOT NULL,
+  data BLOB,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-COMMIT;
