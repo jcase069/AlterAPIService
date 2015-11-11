daa01c86c54b095410181c941080f96281bcf96f
Dan Hedges <daniel.a.hedges@gmail.com>
2015-11-10 21:48:46 -0600

    Switched test to use the async library.  Feels better.

--- test.js
+++ test.js
@@ -1,41 +1,47 @@
-var sqlite = require('./sqlite.js');
-
-var _handleErr= function(err) {
-  if (err) {
-    console.log('Error: '+err);
-    throw err;
-  }
-}
+var sqlite = require('./sqlite.js'),
+  async = require('async');
 
 var testUserCrud = function() {
-  sqlite.addUser({user_name: 'Dan Hedges'}, function (err, val) {
-    if (err) {
-      console.log('Error: '+err);
-      return;
-    } else {
-      var user_id = val;
-      var user = null;
-      sqlite.getUser(user_id, function(err, val) {
-        _handleErr(err);
-        user=val;
-        var user2 = {user_id: user.user_id, user_name: 'Han Dedges'};
-        sqlite.updateUser(user2, function(err, val) {
-          _handleErr(err);
-          var user3 = null;
-          sqlite.getUser(user_id, function(err, val) {
-            _handleErr(err);
-            user3=val;
-            if (user2.user_name == user3.user_name) {
-              console.log('Passed testUserCrud');
-            } else {
-              throw 'Error: Actual ' + user2.user_name + ', Expected ' + user3.user_name;
-            }
-            sqlite.deleteUser(user_id, _handleErr);
-          });
-        });
+  var user_id, user, user2;
+  async.series([
+    // 0. Add a user, put the new id into user_id
+    function(callback) {
+      sqlite.addUser({user_name: 'Dan Hedges'}, function(err,val) {
+        user_id = val;
+        callback(err, val);
       });
+    },
+    // 1. Read the user back, given the user_id
+    function(callback) {
+      sqlite.getUser(user_id, function(err, val) {
+        user = val;
+        callback(err, val);
+      });
+    },
+    // 2. Update the user at user_id with a new user_name.
+    function(callback) {
+      user2 = {user_id: user.user_id, user_name: 'Han Dedges'};
+      sqlite.updateUser(user2, function(err, val) {callback(err);})
+    },
+    // 3. Again, read the user at user_id, then return the result
+    function(callback) {
+      sqlite.getUser(user_id, function(err, val) {callback(err, val);});
+    },
+    // 4. Cleanup.
+    function(callback) {
+      sqlite.deleteUser(user_id, function(err, val) {callback(err);});
     }
-  })
+  ], function(err, results) {
+    if (err) {
+      console.log('Error '+err);
+    } else {
+      if (user2.user_name == results[3].user_name) {
+        console.log('Passed testUserCrud');
+      } else {
+        throw 'Error: Actual ' + user2.user_name + ', Expected ' + user3.user_name;
+      }
+    }
+  });
 }
 
 testUserCrud();
