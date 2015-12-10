578836f54434b045e4f851936ae1c33529e0899d
Dan Hedges <daniel.a.hedges@gmail.com>
2015-12-10 15:04:01 -0600

    Refactored creating a user for testing

--- test.js
+++ test.js
@@ -16,32 +16,40 @@ function compileTestResult(err, results, testName, verificationFunction) {
   }
 }
 
+function createUserForTesting(funcName, params) {
+  return function(callback) {
+    debugger;
+    sqlite.addUser({user_name: 'Test '+funcName}, function (err, val) {
+      debugger;
+      if (!err) {
+        params.user_id = val;
+      }
+      callback(err, val);
+    });
+  };
+}
+
 var testBloodSugarCrud = function(callback) {
-  var user_id, blood_sugar_id;
+  var params={}, blood_sugar_id;
   async.series([
     // 0. Create a user for testing
-    function(callback) {
-      sqlite.addUser({user_name: 'Test User Blood Sugar'}, function(err, val) {
-        user_id = val;
-        callback(err, val);
-      });
-    },
+    createUserForTesting('Blood Sugar Crud', params),
     // 1. Create a blood sugar measurement
     function(callback) {
-      sqlite.addBloodSugar(user_id, 42, function(err, val) {
+      sqlite.addBloodSugar(params.user_id, 42, function(err, val) {
         blood_sugar_id = val;
         callback(err, val);
       });
     },
     // 2. Get the meal measurement
     function(callback) {
-      sqlite.getBloodSugar(user_id, blood_sugar_id, function(err, val) {
+      sqlite.getBloodSugar(params.user_id, blood_sugar_id, function(err, val) {
         callback(err, val.measurement);
       });
     },
     // 3. Update the measurement
     function(callback) {
-      sqlite.updateBloodSugar(user_id, {
+      sqlite.updateBloodSugar(params.user_id, {
         blood_sugar_id: blood_sugar_id,
         measurement_time: '2015-12-10 11:00',
         measurement: 43
@@ -49,15 +57,15 @@ var testBloodSugarCrud = function(callback) {
     },
     // 4. Get back the new measurement
     function(callback) {
-      sqlite.getBloodSugar(user_id, blood_sugar_id, callback);
+      sqlite.getBloodSugar(params.user_id, blood_sugar_id, callback);
     },
     // 5. Delete the measurement
     function(callback) {
-      sqlite.deleteBloodSugar(user_id, blood_sugar_id, callback);
+      sqlite.deleteBloodSugar(params.user_id, blood_sugar_id, callback);
     },
     // 6. Delete the user
     function(callback) {
-      sqlite.deleteUser(user_id, callback);
+      sqlite.deleteUser(params.user_id, callback);
     }
   ], function(err, results) {compileTestResult(err, results, 'testBloodSugarCrud', function(results) {
     if (results[2]==42 && results[4].measurement == 43) {
@@ -70,49 +78,44 @@ var testBloodSugarCrud = function(callback) {
 }
 
 var testMealCrud = function(callback) {
-  var _err=null, user_id, meal_id;
+  var _err=null, params={}, meal_id;
   async.series([
     // 0. First, create a user for testing
-    function(callback) {
-      sqlite.addUser({user_name: 'Test User Meal'}, function(err, val) {
-        user_id = val;
-        callback(err, val);
-      });
-    },
+    createUserForTesting('Meal Crud', params),
     // 1. Create a meal
     function(callback) {
-      sqlite.addMeal(user_id, {est_carbs: 20}, function(err, val) {
+      sqlite.addMeal(params.user_id, {est_carbs: 20}, function(err, val) {
         meal_id = val;
         callback(err, val);
       });
     },
     // 2. Get the meal detail's est_carbs
     function (callback) {
-      sqlite.getMeal(user_id, meal_id, function(err, val) {
+      sqlite.getMeal(params.user_id, meal_id, function(err, val) {
         callback(err, val.est_carbs);
       });
     },
     // 3. Update the meal's est_carbs
     function(callback) {
-      sqlite.updateMeal(user_id, {meal_ID: meal_id, est_carbs: 25, meal_time: '2015-12-04 12:00'}, function(err) {
+      sqlite.updateMeal(params.user_id, {meal_ID: meal_id, est_carbs: 25, meal_time: '2015-12-04 12:00'}, function(err) {
         callback(err, null);
       });
     },
     // 4. Get the meal's new est_carbs
     function(callback) {
-      sqlite.getMeal(user_id, meal_id, function(err, val) {
+      sqlite.getMeal(params.user_id, meal_id, function(err, val) {
         callback(err, val.est_carbs);
       });
     },
     // 5. Delete the meal.
     function(callback) {
-      sqlite.deleteMeal(user_id, meal_id, function(err) {
+      sqlite.deleteMeal(params.user_id, meal_id, function(err) {
         callback(err, null);
       });
     },
     // 6. Delete the user.
     function(callback) {
-      sqlite.deleteUser(user_id, callback);
+      sqlite.deleteUser(params.user_id, callback);
     }
   ], function(err, results) {
     compileTestResult(err, results, 'testMealCrud', function(results){
@@ -129,18 +132,13 @@ var testMealCrud = function(callback) {
 };
 
 var testUserCrud = function(callback) {
-  var user_id, user, user2;
+  var params={}, user, user2;
   async.series([
     // 0. Add a user, put the new id into user_id
-    function(callback) {
-      sqlite.addUser({user_name: 'Dan Hedges'}, function(err,val) {
-        user_id = val;
-        callback(err, val);
-      });
-    },
+    createUserForTesting('User Crud', params),
     // 1. Read the user back, given the user_id
     function(callback) {
-      sqlite.getUser(user_id, function(err, val) {
+      sqlite.getUser(params.user_id, function(err, val) {
         user = val;
         callback(err, val);
       });
@@ -158,11 +156,11 @@ var testUserCrud = function(callback) {
     },
     // 4. Again, read the user at user_id, then return the result
     function(callback) {
-      sqlite.getUser(user_id, function(err, val) {callback(err, val);});
+      sqlite.getUser(params.user_id, function(err, val) {callback(err, val);});
     },
     // 5. Cleanup.
     function(callback) {
-      sqlite.deleteUser(user_id, function(err, val) {callback(err);});
+      sqlite.deleteUser(params.user_id, function(err, val) {callback(err);});
     }
   ], function(err, results) {
     compileTestResult(err, results, 'testUserCrud', function(results) {
