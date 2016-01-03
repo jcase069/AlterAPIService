24fd72a9a39217ebd95c3bbffa4ebde09f8d698e
Dan Hedges <daniel.a.hedges@gmail.com>
2016-01-03 14:49:15 -0600

    Adding ejs for web front-end support

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,5 @@
-exports.render = function (req, res) {
-  res.send('Hello, world');
-}
+exports.render = function(req, res) {
+  res.render('index', {
+    title: 'Hello World'
+  });
+};
--- config/express.js
+++ config/express.js
@@ -19,6 +19,12 @@ module.exports = function() {
   }));
   app.use(bodyParser.json());
 
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+
   require('../app/routes/index.server.route.js')(app);
+
+  app.use(express.static('./public'));
+
   return app;
 };
