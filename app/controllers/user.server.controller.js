0167153551ac4baf878c9431d664f7ffa8991c91
Dan Hedges <daniel.a.hedges@gmail.com>
2016-01-13 20:38:15 -0600

    Allow passing a user name through post params to create user

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -1,9 +1,26 @@
 var User = require("../models/user.server.model.js");
 
+var _validateCreateUserParams = function(user) {
+  if (typeof(user.user_name) != 'string') {
+    return false;
+  }
+  if (user.user_name.length > 16) {
+    return false;
+  }
+  return true;
+}
+
 exports.create = function (req, res, next) {
   // create User
-  console.log("Creating test user");
-  User.add({user_name: 'TestAddUser'}, function(err, user_id) {
+  console.log("Creating user");
+  if (!_validateCreateUserParams(req.body)) {
+    res.render('index', {title: 'Failed due to bad parameters'});
+    return;
+  }
+  var _user = {
+    user_name: req.body.user_name,
+  }
+  User.add(_user, function(err, user_id) {
     if (!err) {
       res.render('index', {title: 'User created: '+user_id});
     } else {
