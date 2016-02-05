45cba7aea9e18fa7b10050b2a94349a981d067c6
Dan Hedges <daniel.a.hedges@gmail.com>
2016-02-05 17:00:36 -0600

    Added example angular controller, and also client-side user authentication

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -2,6 +2,7 @@ exports.render = function(req, res) {
   req.session.lastVisit = new Date();
   res.render('index', {
     title: 'Hello World',
-    userName: req.user ? req.user.user_name : ''
+    user: JSON.stringify(req.user),
+    //userName: req.user ? req.user.user_name : ''
   });
 };
