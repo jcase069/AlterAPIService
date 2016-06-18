c47975a8c204918866c8d80b5a01fc149eb640d2
Dan Hedges <daniel.a.hedges@gmail.com>
2016-06-18 14:01:50 -0500

    Finish taking out obsolete about page routes

--- config/express.js
+++ config/express.js
@@ -39,7 +39,6 @@ module.exports = function() {
 
   require('../app/routes/index.server.route.js')(app);
   require('../app/routes/user.server.route.js')(app);
-  require('../app/routes/about.server.route.js')(app);
   require('../app/routes/meals.server.route.js')(app);
 
   app.use(express.static('./public'));
