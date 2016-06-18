b7ee5511139ec31343904a9486b39706b785a3a1
Dan Hedges <daniel.a.hedges@gmail.com>
2016-06-17 23:23:43 -0500

    Removed dead code from previous about page incarnation

--- app/routes/about.server.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  app.get('/about', function(req, res) {
-    res.render('about', {
-      title: 'About dLogit',
-      user: JSON.stringify(req.user),
-    });
-  });
-};
